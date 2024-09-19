94addaf4d05f01ff9706f6594d236001cc0c9969
Luckykefu <3124568493@qq.com>
2024-09-19 08:45:45 +0800

    Auto-commit at 2024-09-19 08:45:35

--- src/demo_gitautopush.py
+++ src/demo_gitautopush.py
@@ -4,15 +4,16 @@ from .git_auto_push import git_auto_push
 from .log import get_logger
 
 logger = get_logger(__file__)
-output_dir =  "output"
+date_dir =  os.path.join(os.getcwd(), "data")
+os.makedirs(date_dir, exist_ok=True)
 
-PATH_FILE = os.path.join(output_dir, "git_repo_path.txt")
+PATH_FILE = os.path.join(date_dir, "git_repo_path.txt")
 
 
 def read_git_repo_path():
     """从文件中读取 git_repo_path"""
     if os.path.exists(PATH_FILE):
-        with open(PATH_FILE, "r") as file:
+        with open(PATH_FILE, "r", encoding="utf-8") as file:
             return file.read().strip()
     else:
         return "/path/to/your/repo branch"
@@ -20,20 +21,18 @@ def read_git_repo_path():
 
 def write_git_repo_path(path):
     """将 git_repo_path 写入文件"""
-    with open(PATH_FILE, "w") as file:
+    with open(PATH_FILE, "w", encoding="utf-8") as file:
         file.write(path)
         logger.info(f"Write git_repo_path to {PATH_FILE}: {path}")
     return path
 
 
 # 初始化状态变量
-initial_git_repo_path = read_git_repo_path()
 
 
 def demo_gitautopush():
     with gr.Blocks() as gitautopushdemo:
-        git_repo_path_state = gr.State(initial_git_repo_path)  # Store the git_repo_path
-
+        initial_git_repo_path = read_git_repo_path()
         gr.Markdown("## GitAutoPush")
         with gr.Row():
             git_repo_path = gr.Textbox(
@@ -57,7 +56,7 @@ def demo_gitautopush():
             git_auto_push_btn.click(
                 fn=git_auto_push,
                 inputs=[
-                    git_repo_path_state,  # 使用 State 而不是 Textbox
+                    git_repo_path,  # 使用 State 而不是 Textbox
                     git_message,
                     git_username,
                     git_email,
